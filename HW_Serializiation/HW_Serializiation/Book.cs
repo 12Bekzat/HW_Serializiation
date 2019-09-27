50e222b01a5799efeabdd829c99ddf9d2759da1a
Toleutai Bekzat <toleutaib@gmail.com>
2019-09-27 21:23:28 +0600

    Задание 1: добавил класс со своими свойствами

---
+++ HW_Serializiation/HW_Serializiation/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seriazialation
+{
+  [Serializable]
+  public class Book
+  {
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+  }
+}
---
+++ HW_Serializiation/HW_Serializiation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_Serializiation
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+    }
+  }
+}

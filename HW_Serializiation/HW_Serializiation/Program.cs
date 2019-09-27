df19822f734196268de70f047d5511dfb77f2777
Toleutai Bekzat <toleutaib@gmail.com>
2019-09-27 21:24:18 +0600

    Задание 1: заполнил данными класс Book

--- HW_Serializiation/HW_Serializiation/Program.cs
+++ HW_Serializiation/HW_Serializiation/Program.cs
@@ -6,7 +6,39 @@ namespace HW_Serializiation
   {
     static void Main(string[] args)
     {
-
+      List<Book> books = new List<Book>()
+      {
+        new Book {
+          Name = "Robinzon Kruzo",
+          Price = 1000,
+          Author = "Daniel Defo",
+          Year = 1719,
+        },
+        new Book {
+          Name = "Harry Potter",
+          Price = 1500,
+          Author = "JK Rowling",
+          Year = 2000,
+        },
+        new Book {
+          Name = "A Little Life",
+          Price = 3000,
+          Author = "Hanya Yanagihara",
+          Year = 2015,
+        },
+        new Book {
+          Name = "Darkmans",
+          Price = 1300,
+          Author = "Nicola Barker",
+          Year = 2007,
+        },
+        new Book {
+          Name = "Bad Blood",
+          Price = 2000,
+          Author = "Lorna Sage",
+          Year = 2000,
+        },
+      };
     }
   }
 }
